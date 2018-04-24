83a9e34dc3629da1840e3b352153255841205a63
rosexw <rose.xw.wan@gmail.com>
2018-04-24 19:17:25 -0400

    creating a new angular folder with ng new project-name and adding files to this one with generate component

---
+++ angular-tutorial-TD/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CardListComponent } from './card-list/card-list.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
